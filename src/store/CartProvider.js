715653a8a577f56b818a0cc7df55acaa679efc7d
Joey <87529468+cloudyruer@users.noreply.github.com>
2021-12-09 23:41:46 +0800

    reducer added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,7 +8,26 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item)
+    // const updatedItems = state.items.concat(action.item)
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    )
+
+    const existingCartItem = state.items[existingCartItemIndex]
+
+    let updatedItems
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      }
+      updatedItems = [...state.items] // 新記憶體位置
+      updatedItems[existingCartItemIndex] = updatedItem // 複寫掉
+    } else {
+      updatedItems = state.items.concat(action.item)
+    }
+
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount
 
