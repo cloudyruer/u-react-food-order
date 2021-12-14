5073009a956ffc516fb72d8c3ed896f0576fba24
Joey <87529468+cloudyruer@users.noreply.github.com>
2021-12-14 15:51:50 +0800

    connect with DB

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,18 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+
+import { FIREBASE_ORDERS } from '../../config'
 
 import Modal from '../UI/Modal'
 import CartItem from './CartItem'
 import styles from './Cart.module.scss'
 import CartContext from '../../store/cart-context'
+import Checkout from './Checkout'
 
 const Cart = ({ onClose }) => {
+  const [isCheckout, setIsCheckout] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [didSubmit, setDidSubmit] = useState(false)
+
   const cartCtx = useContext(CartContext)
 
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`
@@ -18,6 +25,24 @@ const Cart = ({ onClose }) => {
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem({ ...item, amount: 1 })
+  }
+
+  const orderHandler = () => {
+    setIsCheckout(true)
+  }
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true)
+    await fetch(FIREBASE_ORDERS, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    })
+    setIsSubmitting(false)
+    setDidSubmit(true)
+    cartCtx.clearCart()
   }
 
   const cartItems = (
@@ -35,19 +60,50 @@ const Cart = ({ onClose }) => {
     </ul>
   )
 
-  return (
-    <Modal onClose={onClose}>
+  const modalActions = (
+    <div className={styles.actions}>
+      <button className={styles['button--alt']} onClick={onClose}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={styles.button} onClick={orderHandler}>
+          Order
+        </button>
+      )}
+    </div>
+  )
+
+  const cartModalContent = (
+    <>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={onClose} />
+      )}
+      {!isCheckout && modalActions}
+    </>
+  )
+
+  const isSubmittingModalContent = <p>Sending order data...</p>
+  const didSubmitModalContent = (
+    <>
+      <p>Successfully sent the order!</p>
       <div className={styles.actions}>
-        <button className={styles['button--alt']} onClick={onClose}>
+        <button className={styles.button} onClick={onClose}>
           Close
         </button>
-        {hasItems && <button className={styles.button}>Order</button>}
       </div>
+    </>
+  )
+
+  return (
+    <Modal onClose={onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   )
 }
